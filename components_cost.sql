422ac763e0c850c27a672baaf1774f7ec25f7c3a
gabriel <gabcostadeveloper@gmail.com>
2021-12-16 12:59:56 -0400

    update sql

--- components_cost.sql
+++ components_cost.sql
@@ -1,3 +1,6 @@
-SELECT orderid, totalprice
-FROM componentorder
-WHERE customername = 'DeathStar'
+SELECT CO.orderid, SUM(C.cost) AS TotalPrice
+FROM componentorder CO
+         JOIN component_order_ids COI ON CO.orderid = COI.orderid
+         JOIN component C ON COI.componentid = C.id
+WHERE CO.customername = ''DeathStar''
+GROUP BY CO.orderid
